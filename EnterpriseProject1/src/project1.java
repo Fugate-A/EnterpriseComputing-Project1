04670061f8a20d1bc9008a901295f11ba5b3ba18
Andrew Fugate <andrewfugate1121@yahoo.com>
2024-08-26 17:14:10 -0400

    buttons are added and spread out, not responding to clicks tho

--- EnterpriseProject1/src/project1.java
+++ EnterpriseProject1/src/project1.java
@@ -25,7 +25,15 @@ public class project1 {
 	private static void startGUI()
 	{
 		JFrame startPage = new JFrame();
+		startPage.setLayout(null); //disable layout
+        startPage.setSize(362, 200); //frame size (x, y)
+        startPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+        
+        
+        
+        
+        
 		JButton searchButton = new JButton("Search for item #");
 		JButton viewCartButton = new JButton("View Cart");
 		JButton emptyCart = new JButton("Empty Cart");
@@ -33,12 +41,12 @@ public class project1 {
 		JButton checkoutButton = new JButton("Checkout");
 		JButton exitButton = new JButton("Exit (close app)");
 
-		startPage.add(searchButton);
-		startPage.add(viewCartButton);
-		startPage.add(emptyCart);
-		startPage.add(addItemButton);
-		startPage.add(checkoutButton);
-		startPage.add(exitButton);
+		startPage.add(searchButton).setBounds(1, 0, 150, 30);
+		startPage.add(viewCartButton).setBounds(1, 50, 150, 30);
+		startPage.add(emptyCart).setBounds(1, 100, 150, 30);
+		startPage.add(addItemButton).setBounds(200, 0, 150, 30);
+		startPage.add(checkoutButton).setBounds(200, 50, 150, 30);
+		startPage.add(exitButton).setBounds(200, 100, 150, 30);
 		
 		startPage.setVisible(true);
 	}
